a332f6f1abee750ddd41dc769207a4d3e1f0e35c
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-01-15 00:44:20 +0300

    Removed boilerplate code Implemented OnboardingView

---
+++ Restaurant/Restaurant/OnboardingView.swift
@@ -0,0 +1,98 @@
+//
+//  OnboardingView.swift
+//  Restaurant
+//
+//  Created by Pavel Belenkow on 14.01.2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var showRegisterInvalidMessage = false
+    @State private var errorMessage = ""
+    
+    private let credentialsStorage = CredentialsStorage.shared
+    
+    var body: some View {
+        VStack {
+            TextField("First Name", text: $firstName)
+            TextField("Last Name", text: $lastName)
+            TextField("Email", text: $email)
+                .keyboardType(.emailAddress)
+                .textContentType(.emailAddress)
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+            Button("Register") {
+                validateRegister()
+            }
+        }
+        .padding()
+        .alert(isPresented: $showRegisterInvalidMessage, content: {
+            Alert(
+                title: Text("Error"),
+                message: Text(errorMessage),
+                dismissButton: .default(Text("OK"))
+            )
+        })
+    }
+    
+    private func validateRegister() {
+        let firstNameIsValid = isValid(name: firstName)
+        let lastNameIsValid = isValid(name: lastName)
+        let emailIsValid = isValid(email: email)
+        
+        guard
+            firstNameIsValid && lastNameIsValid && emailIsValid
+        else {
+            var invalidFirstNameMessage = ""
+            if firstName.isEmpty || !isValid(name: firstName) {
+                invalidFirstNameMessage = "First Names can only contain letters and must have at least 3 characters\n\n"
+            }
+            
+            var invalidLastNameMessage = ""
+            if lastName.isEmpty || !isValid(name: lastName) {
+                invalidLastNameMessage = "Last Names can only contain letters and must have at least 3 characters\n\n"
+            }
+            
+            var invalidEmailMessage = ""
+            if !email.isEmpty || !isValid(email: email) {
+                invalidEmailMessage = "The e-mail is invalid and cannot be blank."
+            }
+            
+            self.errorMessage = "Found these errors in the form:\n\n \(invalidFirstNameMessage)\(invalidLastNameMessage)\(invalidEmailMessage)"
+            
+            showRegisterInvalidMessage.toggle()
+            return
+        }
+        
+        credentialsStorage.firstName = firstName
+        credentialsStorage.lastName = lastName
+        credentialsStorage.email = email
+    }
+    
+    private func isValid(name: String) -> Bool {
+        guard !name.isEmpty,
+              name.count > 2
+        else { return false }
+        for chr in name {
+            if (!(chr >= "a" && chr <= "z") && !(chr >= "A" && chr <= "Z") && !(chr == " ") ) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    private func isValid(email: String) -> Bool {
+        guard !email.isEmpty else { return false }
+        let emailValidationRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
+        let emailValidationPredicate = NSPredicate(format: "SELF MATCHES %@", emailValidationRegex)
+        return emailValidationPredicate.evaluate(with: email)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Pavel Belenkow on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
